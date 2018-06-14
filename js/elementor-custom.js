2bebe3ac047f5d8b71194fe80537bf6fccde242b
VINGROUP\dieplk <inwave2>
2018-06-14 17:16:03 +0700

    add jquery product slider

--- js/elementor-custom.js
+++ js/elementor-custom.js
@@ -10,10 +10,23 @@
     };
     /* End element slider */
 
+    /* Start element slides product  */
+    let ElementProductCarouselSlider   =   function( $scope, $ ) {
+
+        let element_product_slides = $scope.find('.element-product-slider__warp');
+
+        $( document ).general_multi_owlCarouse( element_product_slides );
+
+    };
+    /* End element slides product  */
+
     $( window ).on( 'elementor/frontend/init', function() {
 
         /* Element slider */
         elementorFrontend.hooks.addAction( 'frontend/element_ready/slides-theme.default', ElementCarouselSlider  );
+
+        /* Element slides product  */
+        elementorFrontend.hooks.addAction( 'frontend/element_ready/product-slider.default', ElementProductCarouselSlider  );
 
     } );
 
