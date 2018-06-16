ef5d9ce2f38b80d56a495a0a9bad8d312f4c1826
VINGROUP\dieplk <inwave2>
2018-06-16 12:38:32 +0700

    update code

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,11 @@
 
 <?php if( is_active_sidebar("shoptheme-sidebar") ): ?>
 
-    <div class="tz-sidebar col-md-3">
-        <?php dynamic_sidebar("shoptheme-sidebar"); ?>
+    <div class="col-md-3">
+        <div class="sidebar-content">
+            <?php dynamic_sidebar("shoptheme-sidebar"); ?>
+        </div>
+
     </div>
 
 <?php endif; ?>
