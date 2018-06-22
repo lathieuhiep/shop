842d19a47dd965ddab5fc40ca46931e56219df10
VINGROUP\dieplk <inwave2>
2018-06-22 17:39:10 +0700

    create file js

---
+++ js/shop-cat.js
@@ -0,0 +1,32 @@
+/**
+ * shop cat js v1.0.0
+ * Copyright 2017-2020
+ * Licensed under  ()
+ */
+
+( function( $ ) {
+
+    "use strict";
+
+    $( '.product_vendor_check, .product_collection_check' ).on( 'click', function () {
+
+        let vendors = [],
+            collections = [];
+
+        $.each( $('input[data-filter="product_vendor"]:checked'), function () {
+
+            vendors.push($(this).val());
+
+        });
+
+        $.each( $('input[data-filter="product_collections"]:checked'), function () {
+
+            collections.push($(this).val());
+
+        });
+
+        $('#txtValue').val(vendors + collections)
+
+    } )
+
+} )( jQuery );
