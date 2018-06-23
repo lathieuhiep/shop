89710c5c1dd48d0fce31afff1c61ad1e925b7740
VINGROUP\dieplk <inwave2>
2018-06-23 12:16:55 +0700

    update code

--- extension/woocommerce/woo-template-hooks.php
+++ extension/woocommerce/woo-template-hooks.php
@@ -24,7 +24,6 @@ remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
 
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 
-
 add_action( 'woocommerce_before_main_content', 'shoptheme_woo_before_main_content', 10 );
 
 add_action( 'woocommerce_before_shop_loop', 'shoptheme_woo_before_shop_loop_open',  5 );
